6972168d8658141575bd088109d9c5b887588a5c
Francisco Pinto Moreira Neto <31639140+frapimoneto@users.noreply.github.com>
2020-06-04 23:34:47 -0300

    .

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,15 +2,33 @@ import express from 'express';
 
 const app = express();
 
+const users = [
+  'Francisco',
+  'Diego',
+  'Cleiton',
+  'Daniel'
+]
+
 app.get('/users', (request, response) => {
   console.log('Listagem de usuários');
 
-  response.json([
-    'Francisco',
-    'Diego',
-    'Cleiton',
-    'Daniel'
-  ]);
+  return response.json(users);
 });
+
+app.get('/users/:id', (request, response) => {
+  console.log('Listagem de usuários');
+
+  return response.json(users);
+});
+
+app.post('/users', (request, response) => {
+  const user = {
+    name: 'Francisco',
+    email: 'frapimoneto@gmail.com'
+  };
+
+  return response.json(user);
+})
+
 
 app.listen(3333);
